188752718d6f7e41456dcecd6b29fbaf868ddbff
alvastern <alva.stern02@gmail.com>
2026-04-05 11:35:22 +0200

    Skapade HTML-grund med ejs-filer

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');  // Inkluderar express-paketet
 const app = express();  // Startar en express-applikation
 const port = 3000;  // Definierar porten som servern kommer att lyssna på
+app.use(express.static('public'));  // Gör att servern kan hantera statiska filer i mappen "public"
+app.set('view engine', 'ejs');
 
 // Routing - hanterar get-anrop till URL
 app.get("/", (req, res) => {
-    res.send("Tjena!");
+    res.render("index");
 });
 
 // Starta applikationen - vilken port servern ska lyssna på
